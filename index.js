f78b051ce758b2d3dc73e3e5587e9d1dccb3f33f
James White <james@joincatapult.com>
2017-11-30 14:02:28 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const Requests = require('./lib/request');
+
+// interaction channels
+const Telegram = require('./lib/telegram');
+const Speak = require('./lib/speak');
+
+const app = function(){
+  app.requests = new Requests();
+  app.telegram = new Telegram(app);
+  app.speak = new Speak(app);
+}
+
+app();
